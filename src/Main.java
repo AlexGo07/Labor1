185a5d9c09f6a421854c0ed818faf22c1754b01b
Alex <gorganalexandru3@gmail.com>
2024-10-08 22:51:50 +0300

    Aufgabe 1 a)

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 
 public class Main {
-    public static void main(String[] args) {
-
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("Sa vedem");
-            System.out.println("i = " + i);
+    public static void main(String[] args) throws IllegalAccessException {
+        int[] grades = {29, 37, 38, 41, 84, 67};
+        int[] badGrades = University.insufficient(grades);
+        System.out.print("Nicht ausreichende Noten: ");
+        for(int i=0;i < badGrades.length;i++) {
+            System.out.print(badGrades[i] + " ");
         }
+        System.out.println();
     }
 }
 
--- src/University.java
+++ src/University.java
@@ -1,2 +1,21 @@
 public class University {
+    //1.
+    public static int[] insufficient(int[] grades) throws IllegalAccessException {
+        int count = 0;
+        for(int i=0;i<grades.length;i++)
+        {
+            if(grades[i] > 100 && grades[i] < 0)
+                throw new IllegalAccessException("incompatible grade");
+            else if(grades[i] < 40)
+                count++;
+        }
+        int[] badGrades = new int[count];
+        for(int i=0;i<badGrades.length;i++)
+        {
+            if(grades[i] < 40)
+                badGrades[i]=grades[i];
+        }
+        return badGrades;
+    }
+    //2.
 }
